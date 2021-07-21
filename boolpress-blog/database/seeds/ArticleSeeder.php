39fc326303e3df67e87aca4b20981728611d2728
BeaQ17 <beaq235@gmail.com>
2021-07-21 16:31:09 +0200

    fakers (15)

--- boolpress-blog/database/seeds/ArticleSeeder.php
+++ boolpress-blog/database/seeds/ArticleSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Article;
+use Faker\Generator as Faker;
 
 class ArticleSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class ArticleSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $article = new Article();
+            $article->title = $faker->words(4, true);
+            $article->subtitle = $faker->sentence();
+            $article->date = $faker->dateTimeInInterval('-2 years', '+1 days');
+            $article->author = $faker->firstName($gender = null|'male'|'female');
+            $article->five_w = $faker->words(2, true);
+            $article->how_text = $faker->paragraphs(6, true);
+            $article->save();
+        }
     }
 }
