051d6c76e64cddddad21f677424b8c5c11e5f708
BeaQ17 <beaq235@gmail.com>
2021-07-26 01:31:59 +0200

    problem with use Illuminate\http\request;

--- boolpress-blog/app/Http/Controllers/ContactController.php
+++ boolpress-blog/app/Http/Controllers/ContactController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contact;
+use App\Mail\ContactFormMarkdown;
+//use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    //
+    public function form()
+    {
+        return view("guest.contacts");
+    }
 }
