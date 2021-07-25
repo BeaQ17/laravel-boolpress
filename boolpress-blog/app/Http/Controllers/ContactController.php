9cb3b9c55cad0e7886ac7f2ba0d5b9d09f6af8b2
BeaQ17 <beaq235@gmail.com>
2021-07-26 01:37:23 +0200

    contactcontroller

--- boolpress-blog/app/Http/Controllers/ContactController.php
+++ boolpress-blog/app/Http/Controllers/ContactController.php
@@ -14,4 +14,29 @@ class ContactController extends Controller
     {
         return view("guest.contacts");
     }
+
+    public function storeAndSend(Request $request)
+    {
+        //valida dati
+        $validateData = $request->validate([
+            "name" => "required",
+            "email" => "required | email",
+            "message" => "required"
+        ]);
+
+        //salva in database
+        $contact = Contact::create($validateData);
+        //ddd($validatedData);
+        //ddd($contact);
+        //Visualizza senza inviare
+        //return (new ContactFormMarkdown($contact))->render();
+
+        //Invia mail
+        Mail::to("admin@test.com")->send(new ContactFormMarkdown($contact));
+        return redirect()
+            ->back()
+            ->with("message", "You did it! We'll be back ASAP");
+    }
 }
+
+    
