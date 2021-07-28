948eca88eecb0b53a725d2b603831f1a43944634
BeaQ17 <beaq235@gmail.com>
2021-07-28 23:39:05 +0200

    article model

--- boolpress-blog/app/Article.php
+++ boolpress-blog/app/Article.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $fillable = ["title", "subtitle", "date", "author", "image", "text"];
+
+    /**
+     * Get category that owns Article
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
