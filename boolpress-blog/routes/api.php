c55663aca0c72d2111e5a8532a45c6971a7f48c1
BeaQ17 <beaq235@gmail.com>
2021-08-02 00:38:21 +0200

    api routes + api controller + resource

--- boolpress-blog/routes/api.php
+++ boolpress-blog/routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get("post", function () {
+    $articles = Article::all(); 
+    return response()->json([
+        "response" => $articles
+    ]);
+});
+
+Route::get("articles", "API\ArticleController@index");
